dea0c90076b4bded939a376fa767a290519f1aab
Eli Sterling <eli@eliware.org>
2026-09-13 16:26:02 -0400

    Reindex E-1.3 convention child

--- src/checks/general/E-1/E-1.3/A-1.3.1.mjs
+++
@@ -1,8 +0,0 @@
-import { pass } from "../../../check-result.mjs";
-
-export const ruleId = "A-1.3.1";
-export const parentRuleId = "E-1.3";
-
-export function run() {
-  return pass(ruleId);
-}
--- tests/checks/general/E-1/E-1.3/A-1.3.1.test.mjs
+++
@@ -1,5 +0,0 @@
-import { expect, test } from "@jest/globals";
-
-test("placeholder check test", () => {
-  expect(true).toBe(true);
-});
